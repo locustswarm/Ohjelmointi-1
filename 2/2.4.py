283a7e0d461d6def5b5df64f05caaebf71e21564
Miika <miikatol@metropolia.fi>
2026-08-18 15:06:32 +0300

    2.4 done

---
+++ 2/2.4.py
@@ -0,0 +1,8 @@
+luku1 = int(input('Ensimmäinen luku? '))
+luku2 = int(input('Toinen luku? '))
+luku3 = int(input('Kolmas luku? '))
+
+print(f'Summa: {luku1+luku2+luku3} '
+      f'Tulo: {luku1*luku2*luku3} '
+      f'Keskiarvo: {luku1+luku2+luku3/3}'
+      )
