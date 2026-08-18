df882bf2afb602f0b090d73539c6c5aa5fe9328c
Miika <miikatol@metropolia.fi>
2026-08-18 14:59:21 +0300

    2.3 done

--- 2/2.3.py
+++ 2/2.3.py
@@ -1,4 +1,4 @@
 kanta = int(input('kanta? '))
 korkeus = int(input('korkeus? '))
 
-print(f'pinta ala on {kanta * korkeus} cm')
+print(f'pinta ala on {kanta * korkeus} cm ja sen piiri on {2 * (kanta + korkeus)}')
